62a9cb5b95d5b3357f5c6ee3967c8b3d4c292278
Shubham Sharma <sshubham.2003@gmail.com>
2024-01-04 21:37:52 +0530

    added date and time in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,24 @@
-import Chevron from "./icons/SourceControlIcon";
-import ErrorIcon from "./icons/ErrorIcon";
-import WarningIcon from "./icons/WarningIcon";
-import BellIcon from "./icons/BellIcon";
-import CheckIcon from "./icons/CheckIcon";
-import ReactIcon from "./icons/ReactIcon";
+import React, { useState, useEffect } from 'react';
+import Chevron from './icons/SourceControlIcon';
+import ErrorIcon from './icons/ErrorIcon';
+import WarningIcon from './icons/WarningIcon';
+import BellIcon from './icons/BellIcon';
+import CheckIcon from './icons/CheckIcon';
+import ReactIcon from './icons/ReactIcon';
 
 function Footer() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    // Update the current time every second
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Clear the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="flex h-8 justify-between  bg-bottombarBg px-4 text-lg text-textColor max-sm:text-sm">
       <div className="flex items-center gap-x-3">
@@ -29,6 +42,8 @@ function Footer() {
         <div className=" cursor-pointer hover:bg-bottombarHoverBg">
           <BellIcon />
         </div>
+        {/* Display current date and time */}
+        <div>{currentTime.toLocaleString()}</div>
       </div>
     </div>
   );
