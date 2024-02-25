ce3c539e8448487742fa5bf5ff49c2d93b122ec3
Shubham Sharma <sshubham.2003@gmail.com>
2024-02-25 23:48:45 +0530

    Added description for each certification done

--- src/features/certification/certification.jsx
+++ src/features/certification/certification.jsx
@@ -28,8 +28,8 @@ function certification() {
             <div className="flex ml-3 flex-col">
               <div className="font-bold mt-3 text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
               <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: XYZ123</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: HTML, CSS, JavaScript, React</div>
+              <div className="text-gray-300 text-sm mb-1 ">Credential ID: UC-7cabeb5b-b9d3-433c-986-0660016d8aaa</div>
+              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: MongoDB, NodeJS, JavaScript, React, ExpressJS</div>
             </div>
           </div>
         </div>
@@ -38,10 +38,9 @@ function certification() {
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
             <img src={postman} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
-              <div className="font-bold text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
-              <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: XYZ123</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: HTML, CSS, JavaScript, React</div>
+              <div className="font-bold text-white text-3xl">Postman API Fundamentals Student Expert</div>
+              <div className="text-gray-300 text-sm mb-1 ">Postman</div>
+              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: RESTful api, API Key Authorization, scripting</div>
             </div>
           </div>
         </div>
@@ -50,10 +49,10 @@ function certification() {
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
             <img src={ibm} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
-              <div className="font-bold text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
-              <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: XYZ123</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: HTML, CSS, JavaScript, React</div>
+              <div className="font-bold text-white text-3xl">IBM Cloud Essentials V3</div>
+              <div className="text-gray-300 text-sm mb-1 ">IBM</div>
+              <div className="text-gray-300 text-sm mb-1 ">Credential ID: acec26aecabc4544a48ba27a55460492</div>
+              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: Cloud Fundamentals</div>
             </div>
           </div>
         </div>
@@ -62,10 +61,9 @@ function certification() {
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
             <img src={goldman} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
-              <div className="font-bold text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
-              <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: XYZ123</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: HTML, CSS, JavaScript, React</div>
+              <div className="font-bold text-white text-3xl">Goldman Sachs - Software Engineering Job Simulation by Forage</div>
+              <div className="text-gray-300 text-sm mb-1 ">Forage</div>
+              <div className="text-gray-300 text-sm mb-1 ">Credential ID: EZry52PPQ7FoaNePM</div>
             </div>
           </div>
         </div>
@@ -75,10 +73,10 @@ function certification() {
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
             <img src={jp} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
-              <div className="font-bold text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
-              <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: XYZ123</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: HTML, CSS, JavaScript, React</div>
+              <div className="font-bold text-white text-3xl">J.P. Morgan - Software Engineering Job Simulation by Forage</div>
+              <div className="text-gray-300 text-sm mb-1 ">Forage</div>
+              <div className="text-gray-300 text-sm mb-1 ">Credential ID: rr236C96qrvCSvZq9</div>
+              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: OpenSource</div>
             </div>
           </div>
         </div>
@@ -88,10 +86,9 @@ function certification() {
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
             <img src={scalar} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
-              <div className="font-bold text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
-              <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: XYZ123</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: HTML, CSS, JavaScript, React</div>
+              <div className="font-bold text-white text-3xl">DBMS Course - Master the Fundamentals and Advanced Concepts</div>
+              <div className="text-gray-300 text-sm mb-1 ">Scalar</div>
+              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: SQL, Data Base Management</div>
             </div>
           </div>
         </div>
@@ -100,10 +97,10 @@ function certification() {
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
             <img src={spoken} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
-              <div className="font-bold text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
-              <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: XYZ123</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: HTML, CSS, JavaScript, React</div>
+              <div className="font-bold text-white text-3xl">Advanced Cpp Training - Spoken Tutorial Project, IIT Bombay</div>
+              <div className="text-gray-300 text-sm mb-1 ">Spoken Tutorial</div>
+              <div className="text-gray-300 text-sm mb-1 ">Credential ID: 30193721KC</div>
+              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: C++</div>
             </div>
           </div>
         </div>
@@ -113,9 +110,9 @@ function certification() {
             <img src={maven} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
               <div className="font-bold text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
-              <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: XYZ123</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: HTML, CSS, JavaScript, React</div>
+              <div className="text-gray-300 text-sm mb-1 ">Maven Silicon</div>
+              <div className="text-gray-300 text-sm mb-1 ">Credential ID: MS/VIT-DM+DI/SEP185</div>
+              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: Chip Architecture, VLSI, Hardware Description Language</div>
             </div>
           </div>
         </div>
