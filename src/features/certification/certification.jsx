74f961bb726a5c1bdd94f2a6fbb791eafffccf1b
Shubham Sharma <sshubham.2003@gmail.com>
2024-02-26 00:27:30 +0530

    added blur and button style

--- src/features/certification/certification.jsx
+++ src/features/certification/certification.jsx
@@ -7,6 +7,7 @@ import ibm from "../../assets/images/ibm.png";
 import maven from "../../assets/images/maven.png";
 import spoken from "../../assets/images/spoken.jpeg";
 import jp from "../../assets/images/jp.jpeg";
+import "../../features/certification/certification.css"
 
 function certification() {
 
@@ -22,32 +23,46 @@ function certification() {
           </div>
         </div>
 
-        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000">
-          <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
-            <img src={udemy} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
-            <div className="flex ml-3 flex-col">
-              <div className="font-bold mt-3 text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
-              <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
-              <div className="text-gray-300 text-sm mb-1 ">Credential ID: UC-7cabeb5b-b9d3-433c-986-0660016d8aaa</div>
-              <div className="text-gray-300 text-sm mb-1 ">Skills Learned: MongoDB, NodeJS, JavaScript, React, ExpressJS</div>
-            </div>
+        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000 custom-zoom-effect relative">
+        <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
+          <img src={udemy} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
+          <div className="flex ml-3 flex-col">
+            <div className="font-bold mt-3 text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
+            <div className="text-gray-300 text-sm mb-1 ">Udemy</div>
+            <div className="text-gray-300 text-sm mb-1 ">Credential ID: UC-7cabeb5b-b9d3-433c-986-0660016d8aaa</div>
+            <div className="text-gray-300 text-sm mb-1 ">Skills Learned: MongoDB, NodeJS, JavaScript, React, ExpressJS</div>
           </div>
         </div>
 
-        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000">
+        {/* Blur overlay and button */}
+          <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out blur-bg">
+            <a href="https://www.udemy.com/certificate/UC-7cabeb5b-b9d3-433c-986f-0660016d8aaa/" target="_blank" rel="noopener noreferrer" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white  dark:text-gray-900 dark:hover:bg-gray-200 me-2 mb-2">
+              View Credential
+            </a>
+          </div>
+        </div>
+
+
+        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000 custom-zoom-effect relative">
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
-            <img src={postman} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
+            <img src={postman} className="h-[180px] w-[180px] mb-3 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
               <div className="font-bold text-white text-3xl">Postman API Fundamentals Student Expert</div>
               <div className="text-gray-300 text-sm mb-1 ">Postman</div>
               <div className="text-gray-300 text-sm mb-1 ">Skills Learned: RESTful api, API Key Authorization, scripting</div>
             </div>
           </div>
+         {/* Blur overlay and button */}
+         <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out blur-bg">
+            <a href="https://badgr.com/public/assertions/piPQu4NaSDOCrvk134zzCg?identity__email=shubham8186@gmail.com" target="_blank" rel="noopener noreferrer" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white  dark:text-gray-900 dark:hover:bg-gray-200 me-2 mb-2">
+              View Credential
+            </a>
+          </div>
         </div>
 
-        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000">
+        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000 custom-zoom-effect relative">
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
-            <img src={ibm} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
+            <img src={ibm} className="h-[180px] w-[180px] mb-3 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
               <div className="font-bold text-white text-3xl">IBM Cloud Essentials V3</div>
               <div className="text-gray-300 text-sm mb-1 ">IBM</div>
@@ -55,23 +70,35 @@ function certification() {
               <div className="text-gray-300 text-sm mb-1 ">Skills Learned: Cloud Fundamentals</div>
             </div>
           </div>
+          {/* Blur overlay and button */}
+          <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out blur-bg">
+            <a href="https://courses.yl-ptech.skillsnetwork.site/certificates/acec26aecabc4544a48ba27a55460492" target="_blank" rel="noopener noreferrer" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white  dark:text-gray-900 dark:hover:bg-gray-200 me-2 mb-2">
+              View Credential
+            </a>
+          </div>
         </div>
 
-        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000">
+        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000 custom-zoom-effect relative">
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
-            <img src={goldman} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
+            <img src={goldman} className="h-[180px] w-[180px] mb-3 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
               <div className="font-bold text-white text-3xl">Goldman Sachs - Software Engineering Job Simulation by Forage</div>
               <div className="text-gray-300 text-sm mb-1 ">Forage</div>
               <div className="text-gray-300 text-sm mb-1 ">Credential ID: EZry52PPQ7FoaNePM</div>
             </div>
           </div>
+          {/* Blur overlay and button */}
+          <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out blur-bg">
+            <a href="https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Goldman%20Sachs/NPdeQ43o8P9HJmJzg_Goldman%20Sachs_GqcERjaMFuukfd4h2_1701362838720_completion_certificate.pdf" target="_blank" rel="noopener noreferrer" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white  dark:text-gray-900 dark:hover:bg-gray-200 me-2 mb-2">
+              View Credential
+            </a>
+          </div>
         </div>
 
 
-        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000">
+        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000 custom-zoom-effect relative">
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
-            <img src={jp} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
+            <img src={jp} className="h-[180px] w-[180px] mb-3 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
               <div className="font-bold text-white text-3xl">J.P. Morgan - Software Engineering Job Simulation by Forage</div>
               <div className="text-gray-300 text-sm mb-1 ">Forage</div>
@@ -79,12 +106,18 @@ function certification() {
               <div className="text-gray-300 text-sm mb-1 ">Skills Learned: OpenSource</div>
             </div>
           </div>
+          {/* Blur overlay and button */}
+          <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out blur-bg">
+            <a href="https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/J.P.%20Morgan/R5iK7HMxJGBgaSbvk_J.P.%20Morgan_GqcERjaMFuukfd4h2_1701767679246_completion_certificate.pdf" target="_blank" rel="noopener noreferrer" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white  dark:text-gray-900 dark:hover:bg-gray-200 me-2 mb-2">
+              View Credential
+            </a>
+          </div>
         </div>
 
 
-        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000">
+        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000 custom-zoom-effect relative">
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
-            <img src={scalar} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
+            <img src={scalar} className="h-[180px] w-[180px] mb-3 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
               <div className="font-bold text-white text-3xl">DBMS Course - Master the Fundamentals and Advanced Concepts</div>
               <div className="text-gray-300 text-sm mb-1 ">Scalar</div>
@@ -93,9 +126,9 @@ function certification() {
           </div>
         </div>
 
-        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000">
+        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000 custom-zoom-effect relative">
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
-            <img src={spoken} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
+            <img src={spoken} className="h-[180px] w-[180px] mb-3 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
               <div className="font-bold text-white text-3xl">Advanced Cpp Training - Spoken Tutorial Project, IIT Bombay</div>
               <div className="text-gray-300 text-sm mb-1 ">Spoken Tutorial</div>
@@ -105,9 +138,9 @@ function certification() {
           </div>
         </div>
 
-        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000">
+        <div className="flex max-w-full mt-3 flex-col sm:flex-row rounded-xl bg-articleBg text-textColor hover:shadow-lg animate-in fade-in zoom-in delay-0 duration-1000 custom-zoom-effect relative">
           <div className="p-3 mt-2 mb-2 ml-2 flex flex-col sm:flex-row items-center">
-            <img src={maven} className="h-[180px] w-[180px] mb-1 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
+            <img src={maven} className="h-[180px] w-[180px] mb-3 sm:mr-2 sm:mb-0 rounded-b-xl rounded-t-md object-fill sm:self-center" alt="Certifications" />
             <div className="flex ml-3 flex-col">
               <div className="font-bold text-white text-3xl">The Complete 2023 Web Development Bootcamp</div>
               <div className="text-gray-300 text-sm mb-1 ">Maven Silicon</div>
