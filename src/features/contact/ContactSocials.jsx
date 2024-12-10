04466fdb8e176026482dbabf3fad918feb0665fb
Shubham Sharma <sshubham.2003@gmail.com>
2024-12-10 09:38:53 +0530

    improved social link Css

--- src/features/contact/ContactSocials.jsx
+++ src/features/contact/ContactSocials.jsx
@@ -27,35 +27,38 @@ function ContactSocials() {
   ];
   return (
     <div className="line-container flex flex-col">
-      {/* HyperText for "Reach Out Via Socials" */}
-      <HyperText
-        className="text-3xl text-white"
-        text="Reach Out Via Socials"
-      />
-      <div className="flex flex-col space-y-2 pt-5 animate-in fade-in zoom-in delay-0 duration-1000 ">
-        <p className="line text-base text-textColor md:text-2xl ">
-          .socials &#123;{" "}
-        </p>
-        {socials.map((social) => {
-          return (
-            <div className="  line text-base md:text-2xl" key={social.social}>
-              <span className="pl-5 text-textColor md:pl-8">
-                {social.social}:
-              </span>
-              <a
-                className=" pl-2 text-base text-accentColor hover:underline md:text-2xl"
-                href={social.href}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {social.un};
-              </a>
-            </div>
-          );
-        })}
-        <p className="line text-base text-textColor md:text-2xl">&#125;</p>
-      </div>
+    {/* HyperText for "Reach Out Via Socials" */}
+    <HyperText className="text-3xl text-white" text="Reach Out Via Socials" />
+    <div className="flex flex-col space-y-2 pt-5 animate-in fade-in zoom-in delay-0 duration-1000">
+      <p className="line text-base text-textColor md:text-2xl">
+        <span style={{ color: "#C792EA" }}>.socials</span> &#123;
+      </p>
+      {socials.map((social) => {
+        return (
+          <div className="line text-base md:text-2xl" key={social.social}>
+            <span
+              className="pl-5 md:pl-8"
+              style={{ color: "#82AAFF" }}
+            >
+              {social.social}
+            </span>
+            <span style={{ color: "#89DDFF" }}>:</span>
+            <a
+              className="pl-2 hover:underline"
+              style={{ color: "#C3E88D" }}
+              href={social.href}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {social.un}
+            </a>
+            <span style={{ color: "#89DDFF" }}>;</span>
+          </div>
+        );
+      })}
+      <p className="line text-base text-textColor md:text-2xl">&#125;</p>
     </div>
+  </div>
   );
 }
 
